b9251164deb8dba1079b85ff93d816706d0ec640
Taali <matrebacz125@gmail.com>
2024-07-07 23:22:36 +0200

    1

--- presta/presta/main.py
+++ presta/presta/main.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from dotenv import load_dotenv
 import os
-from presta import get_orders
+from presta_import import get_orders
 
 # Loading environmental variables
 load_dotenv()
@@ -10,7 +10,7 @@ PRESTA_API = os.getenv('PRESTA_API')
 def list_view(request):
     orders =  get_orders(PRESTA_API)
     context = {'orders': orders}
-    return render(request, 'presta/index.html', context)
+    return render(request, 'presta/templates/index.html', context)
 
 
 
