2e4180e901c6fec4196b19367a6700b7940dfb10
Taali <matrebacz125@gmail.com>
2024-07-07 11:48:01 +0200

    2

--- presta.py
+++ presta.py
@@ -29,7 +29,8 @@ def get_order_request(api_key: str, order_id):
     response = requests.get(url, headers=headers, auth=HTTPBasicAuth(api_key, ''))
 
     if response.status_code == 200:
-        return response.json()
+        return response.json()['order']
+
     else:
         print(f"Error: {response.status_code}")
         print(response.text)
@@ -38,19 +39,44 @@ def get_order_request(api_key: str, order_id):
 def get_orders(api_key: str):
     result = []
     id_list = get_orders_request(api_key)['orders']
+    columns = ['id', 'id_address_delivery', 'id_cart', 'id_address_invoice', 'id_customer', 'current_state', 'valid', 'date_add', 'note', 'total_paid_real', 'associations']
 
     for order_id in id_list:
         result += [get_order_request(api_key, order_id['id'])]
         print(f'{order_id["id"]}: Pass')
 
     df = pd.DataFrame(result)
-    columns = ['id', 'id_address_delivery', 'id_cart', 'id_address_invoice', 'id_customer', 'current_state', 'valid', 'date_add', 'note', 'total_paid_real', 'associations']
-    df = df[columns]
+    return df[columns]
 
-    return df 
+def write_orders_csv(df_orders):
+    print('df written orders data')
+    df_orders.to_csv('orders_data.txt', sep='\t', index=False)
+
+def get_clients(api_key: str, customer_id):
+    url = f"{base_url}/customers/{customer_id}"
+    headers = {'Output-Format': 'JSON'}
+
+    response = requests.get(url, headers=headers, auth=HTTPBasicAuth(api_key, ''))
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: {response.status_code}")
+        print(response.text)
+        return None
+
+def get_address(api_key: str, address_id):
+    url = f"{base_url}/address/{address_id}"
+    headers = {'Output-Format': 'JSON'}
+
+    response = requests.get(url, headers=headers, auth=HTTPBasicAuth(api_key, ''))
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: {response.status_code}")
+        print(response.text)
+        return None
 
 
-df = get_orders(PRESTA_API)
-print('df written')
-
-df.to_csv('orders_data.txt', sep='\t', index=False)
+write_orders_csv(get_orders(PRESTA_API))
