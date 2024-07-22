a86e8e0d7e62f9db5de83abca92c89682ad2d701
Taali <matrebacz125@gmail.com>
2024-07-22 22:17:15 +0200

    1.0

--- presta/presta/inpost.py
+++ presta/presta/inpost.py
@@ -2,17 +2,14 @@ import requests
 import os
 from requests.auth import HTTPBasicAuth
 from dotenv import load_dotenv
-import pandas as pd
 import json
+from presta_import import *
+
 
 # Loading environmental variables
 load_dotenv()
 INPOST_API = os.getenv('INPOST_API')
 base_url = 'https://api-shipx-pl.easypack24.net/'
-
-
-
-
 
 def post_inpost(api_key: str) -> json:
     url = base_url + '/v1/organizations/73164/shipments'
@@ -26,16 +23,34 @@ def post_inpost(api_key: str) -> json:
     }
 
     requests.post(url, headers=headers)
-def set_receiver(receiver: str, parcels: list, cod, insurance: ):
 
+def create_post_request(
+    mpk: str,                                       # skąd ma pobierać opłatę z przesyłkę
+    receiver: json,                                 #
+    sender: json,                                   #
+    parcels: json,                                  # [form] - cechy paczki (wymiary, waga)
+    cod: json,                                      # [form] - wartość z pobraniem 
+    insurance: json,                                # [form] - ubezpieczenie
+    reference: str,                                 # dodatkowy opis przesyłki, tutaj id zamówienia
+    is_return: bool = False,                        # czy jest to zwort 
+    comments: str = "",                             #
+    only_choice_of_offer: bool = True,              # jeżeli True to tworzy przesyłkę bez opłaty, w inpost defaultowo jest False tzn. tworzenie i opłata
+    additional_services: list = ['sms', 'email']    #
+    ):
 
-    result = {
+    return json.dumps({
+        "reference": reference,
+        "mpk": mpk,
+        "comments": comments,
         "receiver": receiver,
-
-    }
-
-    return result
-
+        "sender": sender,
+        "parcels": parcels,
+        "insurance": insurance,
+        "cod": cod,
+        "additional_services": additional_services,
+        "only_choice_of_offer": only_choice_of_offer,
+        "is_return": is_return
+    })
 
 
 
--- presta/presta/presta_import.py
+++ presta/presta/presta_import.py
@@ -3,6 +3,7 @@ import os
 from requests.auth import HTTPBasicAuth
 from dotenv import load_dotenv
 import pandas as pd
+import json
 
 # Loading environmental variables
 load_dotenv()
@@ -78,5 +79,136 @@ def get_address(api_key: str, address_id):
         print(response.text)
         return None
 
+def create_sender_form(company_name: str, email: str, phone: str, address: json) -> json:
+    try:
+        if address:
+            return json.dumps({
+                "company_name": company_name,
+                "email": email,
+                "phone": phone,
+                "address": address
+            })
+        else:
+            return json.dumps({
+                "company_name": company_name,
+                "email": email,
+                "phone": phone
+            })
+    except Exception as error:
+        print(error)
 
-write_orders_csv(get_orders(PRESTA_API))
+def create_cod_form(amount: int, currency: str = 'PLN') -> json:
+    # in InPost default currency is PLN
+    try:
+        return json.dumps({
+            "amount": amount,
+            "currency": currency
+        })
+    except Exception as error:
+        print(error)
+
+
+def create_insurance(amount: int, currency: str = "PLN") -> json:
+    # in InPost default currency is PLN
+    try:
+        return json.dumps({
+            "amount": amount,
+            "currency": currency
+        })
+    except Exception as error:
+        print(error)
+
+
+def create_parcels_form( height: float, length: float, width: float, weight_amount: float, id: str = "package") -> json:
+    try:    
+        # in InPost default unit is mm
+        # height, length, width [float]
+        dimensions = {
+            "height": height,
+            "length": length,
+            "width": width
+        }
+
+        # in InPost default unit is kg
+        # weight_amount [float]
+        weight = {
+            "weight_amount": weight_amount
+        }
+
+        if dimensions and weight:
+            return json.dumps([{
+                "id": id, # idk losowe id = package
+                "dimensions": dimensions,
+                "wieght": weight
+            }])
+    except Exception as error:
+        print(error)
+
+# wymagane w przypadku paczkomatu
+def create_custom_attributes(target_point: str) -> json:
+    try:
+        return json.dumps({
+            "target_point": target_point
+        })
+    except Exception as error:
+        print(error)
+
+
+def create_address_form(street: str, city: str, post_code: str) -> json:
+# street [str(255)] - wymagane
+# city [str(255)] - wymagane
+# post_code [str(6)] - wymagane (np. 00-000)
+    try:
+        return json.dumps({
+            "street": street,
+            "city": city,
+            "post_code": post_code
+        })
+    except Exception as error:
+        print(error)
+
+def create_receiver_form(company_name: str, first_name: str, last_name: str, phone: str, email: str, address: str) -> json:
+# company_name [str(255)] - wymagany gdy nie jest podany adres, imie, nazwisko
+# first_name [str] - wymagany gdy nie jest podany email, phone, company_name
+# last_name [str] - wymagany gdy nie jest podany email, phone, company_name
+# phone [str(9)] - zawsze wymagane
+# email [str] - wymagany w momencie przesyłki paczkomatem
+# address [form] - wymagany gdy wysyłana kurierem
+    try:
+        if company_name: # wysyłka do firmy
+            if address: # kurier
+                return json.dumps({
+                    "company_name": company_name,
+                    "phone": phone,
+                    "email": email,
+                    "address": address
+                })
+            else: # paczkomat
+                return json.dumps({
+                    "company_name": company_name,
+                    "phone": phone,
+                    "email": email
+                })
+        
+        if first_name and last_name: # wysyłka do osoby prywatnej
+            if address: # kurier
+                return json.dumps({
+                    "first_name": first_name,
+                    "last_name": last_name,
+                    "phone": phone,
+                    "email": email,
+                    "address": address
+                })
+            else: # paczkomat
+                return json.dumps({
+                    "first_name": first_name,
+                    "last_name": last_name,    
+                    "phone": phone,
+                    "email": email
+                })
+    except Exception as error:
+        print(error)
+
+
+
+print(get_orders(PRESTA_API))
